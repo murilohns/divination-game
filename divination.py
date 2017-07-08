27558df55b21d027829084cacbff3ea8a3bc9803
Murilo Henrique <murilohns@gmail.com>
2017-07-08 10:04:56 -0300

    Added the game logic

--- divination.py
+++ divination.py
@@ -1,3 +1,16 @@
 print('------------------------------')
 print('Welcome to the Divination Game')
 print('------------------------------')
+
+secret_number = 42
+
+guess = input("Type your number: ")
+
+print('You typed', guess)
+
+if (guess == secret_number):
+    print('Congratulations, you win!')
+else:
+    print('U LOSE!')
+
+print ("The End")
