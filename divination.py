108af1888a13598a7ce71e3ac3b8d919c9d551db
Murilo Henrique <murilohns@gmail.com>
2017-07-08 10:39:17 -0300

    Refactored the game schema

--- divination.py
+++ divination.py
@@ -1,17 +1,23 @@
+def game():
+    number = input("Type your number: ")
+    number = int(number)
+
+    print('You typed', number)
+
+    if (number == secret_number):
+        print('Congratulations, you win!')
+    else:
+        if (number < secret_number):
+            print('Wrong! Type a GREATER number! ')
+            game()
+        elif (number > secret_number):
+            print('Wrong! Type a LOWER number!')
+            game()
+
 print('------------------------------')
 print('Welcome to the Divination Game')
 print('------------------------------')
 
 secret_number = 42
 
-number = input("Type your number: ")
-number = int(number)
-
-print('You typed', number)
-
-if (number == secret_number):
-    print('Congratulations, you win!')
-else:
-    print('U LOSE!')
-
-print ("The End")
+game()
