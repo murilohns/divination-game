31f2c671f6844ba3b75a35bac1d863693561e505
Murilo Henrique <murilohns@gmail.com>
2017-07-08 11:29:11 -0300

    Refactored game logic

--- divination.py
+++ divination.py
@@ -1,10 +1,18 @@
-def game():
+print('------------------------------')
+print('Welcome to the Divination Game')
+print('------------------------------')
+
+secret_number = 42
+tries = 0
+
+for tries in range (0,3):
     number = input("Type your number: ")
     number = int(number)
 
     correct = number == secret_number
     lower = number < secret_number
     greater = number > secret_number
+
     print('You typed', number)
 
     if (correct):
@@ -12,15 +20,5 @@ def game():
     else:
         if (lower):
             print('Wrong! Type a GREATER number! ')
-            game()
         elif (greater):
             print('Wrong! Type a LOWER number!')
-            game()
-
-print('------------------------------')
-print('Welcome to the Divination Game')
-print('------------------------------')
-
-secret_number = 42
-
-game()
