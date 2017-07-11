542349855a24ad113ba6950194a6b3d8cba3770e
Murilo Henrique <murilohns@gmail.com>
2017-07-10 23:18:34 -0300

    Implemented gallow basic logic

--- gallow.py
+++ gallow.py
@@ -2,6 +2,13 @@ def play():
     print('------------------------------')
     print('Welcome to the Divination Game')
     print('------------------------------')
+    
+    secret_word = "banana"
+    hanged = False
+    correct = False
+    
+    while (not hanged and not correct):
+        print("Playing")
 
     print("The end")
 
