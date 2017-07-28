73537099d083ac26b60eba92b995ff3cef145bb1
Murilo Henrique <murilohns@gmail.com>
2017-07-27 21:05:20 -0300

    Added somes features and forgot to commit :D

--- gallow.py
+++ gallow.py
@@ -9,19 +9,37 @@ def play():
     for i in secret_word:
         values.append("_")
 
+    print (values)
     hanged = False
     correct = False
+    wrongs = 0
     
     while (not hanged and not correct):
         letter = input("Type a letter: ").strip()
 
         index = 0;
+        if (letter not in secret_word):
+            wrongs += 1
+            if (wrongs == 6):
+                hanged = True
+                print("LOOOSER")
+                break;
+                
         for l in secret_word:
             if (l.lower() == letter.lower()):
                 print("Found ", l, "in position", index)
                 values[index] = l
-            index += 1 
-        print(values)
+            index += 1
+
+        word = str();
+        for l in values:
+            word += l + " "
+
+        print(word)
+
+        if ('_' not in values):
+            correct = True
+            print("YOU WIN!!")
 
     print("The end")
 
