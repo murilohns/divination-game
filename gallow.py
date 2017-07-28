391c2efa978c6fbf4cfca3d5e0b3df14afb90d2d
Murilo Henrique <murilohns@gmail.com>
2017-07-27 21:47:17 -0300

    Added somes features and forgot to commit :D

--- gallow.py
+++ gallow.py
@@ -3,7 +3,7 @@ def play():
     print('Welcome to the Divination Game')
     print('------------------------------')
     
-    secret_word = "banana"
+    secret_word = "banana".lower()
     
     values = []
     for i in secret_word:
@@ -13,34 +13,32 @@ def play():
     hanged = False
     correct = False
     wrongs = 0
-    
+
     while (not hanged and not correct):
-        letter = input("Type a letter: ").strip()
+        letter = input("Type a letter: ").strip().lower()
 
         index = 0;
-        if (letter not in secret_word):
+        if (letter in secret_word):
+            for l in secret_word:
+                if (l == letter):
+                    print("Found ", l, "in position", index)
+                    values[index] = l
+                index += 1
+        else:
             wrongs += 1
-            if (wrongs == 6):
-                hanged = True
-                print("LOOOSER")
-                break;
-                
-        for l in secret_word:
-            if (l.lower() == letter.lower()):
-                print("Found ", l, "in position", index)
-                values[index] = l
-            index += 1
+        
+        hanged = wrongs == 6
+        correct = '_' not in values
 
-        word = str();
+        word = str()
         for l in values:
             word += l + " "
 
         print(word)
-
-        if ('_' not in values):
-            correct = True
-            print("YOU WIN!!")
-
+    if(correct):
+        print("YOU ARE THE CHAMPION!!")
+    else:
+        print("YOU LOSE!!")
     print("The end")
 
 if(__name__ == "__main__"):
